adff05a41b841a3e5a36a3648481e18a7ec2ede0
Gustavo Vilela <gutusmao12@hotmail.com>
2022-01-23 20:52:37 -0300

    Upload Guess my Number project

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    // When there is no input
+    if(!guess || guess > 20) {
+        document.querySelector('.message').textContent = 'Not valid! Enter a number between 1 and 20';
+
+        // When player wins   
+    } else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct number! Congratulations!'
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
+        // When guess is wrong
+    } else if(guess !== secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = guess > secretNumber ? 'Too high' : 'Too low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!'
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
